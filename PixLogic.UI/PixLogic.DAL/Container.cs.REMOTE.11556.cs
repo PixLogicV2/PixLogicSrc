f0fcc07c8a07119c6981b25c7b123ef81d817f38
Peuch <lucas.78.13@gmail.com>
2015-11-09 10:30:43 +0100

    dal ajout

---
+++ PixLogic.UI/PixLogic.DAL/Container.cs.REMOTE.11556.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace PixLogic.DAL
+{
+    public class Container
+    {
+        Dictionary<string, dynamic> arrayContainer;
+        public Container()
+        {
+            this.arrayContainer = new Dictionary<string, dynamic>();
+        }
+        public dynamic get(string serviceName)
+        {
+            if (this.arrayContainer.ContainsKey(serviceName))
+            {
+               return this.arrayContainer[serviceName];
+            }
+            dynamic service=null;
+            switch (serviceName)
+            {
+                case "item_factory":
+                    service = new ItemFactory();
+                    break;
+                case "pack_factory":
+                    service = new PackFactory();
+                    break;
+
+                case "add_item":
+                    service = new AjoutItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "add_pack":
+                    service = new AddPack();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "data_context":
+                    service = new DataContext();
+                    break;
+                case "get_all_items":
+                    service = new GetAllItems();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "add_pack_to_item":
+                    service = new AddPackToItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "delete_pack_to_item":
+                    service = new DeletePackToItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "contain_item":
+                    service = new ContainItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "update_item":
+                    service = new UpdateItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "delete_item":
+                    service = new DeleteItem();
+                    service.setContext(this.get("data_context"));
+                    break;
+                case "get_items_in_pack":
+                    service = new GetItemsInPack();
+                    service.setContext(this.get("data_context"));
+                    break;
+
+            }
+            this.arrayContainer[serviceName] = service;
+
+            return this.arrayContainer[serviceName];
+        }
+    }
+}
