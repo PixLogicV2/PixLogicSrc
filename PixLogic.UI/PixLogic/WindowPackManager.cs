bbfe15687f7b9c124a79144f1a36811ba998f18d
JorisNT <jorisnsenguet@gmail.com>
2015-11-13 11:21:58 +0100

    Correction: Mise a jour comboxPax a la suppression d'un pack

--- PixLogic.UI/PixLogic/WindowPackManager.cs
+++ PixLogic.UI/PixLogic/WindowPackManager.cs
@@ -136,6 +136,7 @@ namespace PixLogic
             {
                 database.DeletePack(valNamePack.Text);
                 setTablePacks(database.GetAllPacks());
+                pan.setComboBoxPack();
             }
         }
 
