8629597213289aca460b1f5b2ddd4a1778bb6b51
JorisNT <jorisnsenguet@gmail.com>
2015-11-16 11:47:26 +0100

    Configuration Nunit

---
+++ PixLogic.UI/PixLogic.Test/ClassTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PixLogic.Test
+{
+    [TestFixture]
+    public class ClassTest
+    {
+
+        [Test]
+        public void quickTest()
+        {
+            int result = 2;
+            result += 3;
+            Assert.AreEqual(5, result);
+        }
+    }
+}
