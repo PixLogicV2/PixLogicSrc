b9c5472faf5f2cef33e14f421582d803cf912385
Peuch <lucas.78.13@gmail.com>
2015-12-02 16:06:17 +0100

    categorie description

---
+++ PixLogic.UI/PixLogic.DAL/GetCategorieById.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixLogic.DAL
+{
+    public class GetCategorieById
+    {
+        private DataContext context;
+
+        public void setContext(DataContext context)
+        {
+            this.context = context;
+        }
+        public Categorie getCategorieById(int id)
+        {
+            IQueryable<Categorie> itemQuery = from Categorie in context.Categories
+                                         where Categorie.CategorieId == id
+                                         select Categorie;
+            List<Categorie> list = new List<Categorie>();
+            foreach (var prod in itemQuery)
+            {
+                list.Add(prod);
+            }
+            return list.First();
+        }
+    }
+}
