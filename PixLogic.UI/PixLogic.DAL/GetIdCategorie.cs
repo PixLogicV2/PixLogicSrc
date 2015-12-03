27f661f3494ce1c5c86b428c3a38c1d193a94abb
Peuch <lucas.78.13@gmail.com>
2015-12-03 12:14:40 +0100

    getIdCategorie

---
+++ PixLogic.UI/PixLogic.DAL/GetIdCategorie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixLogic.DAL
+{
+   public class GetIdCategorie
+    {
+        private DataContext context;
+
+        public void setContext(DataContext context)
+        {
+            this.context = context;
+        }
+        public int getIdCategorie(string itemName)
+        {
+            IQueryable<Categorie> itemQuery = from Categorie in context.Categories
+                                         where Categorie.name == itemName
+                                         select Categorie;
+            List<Categorie> list = new List<Categorie>();
+            foreach (var prod in itemQuery)
+            {
+                list.Add(prod);
+            }
+            return list.First().CategorieId;
+        }
+    }
+}
