345d83afa70313e5b49bcc733aca42b25a6c3139
Benoit Humblot <benoit.humblot@orange.fr>
2015-12-05 17:18:30 +0100

    Implémentation de l'export du résultat des requetes en csv

--- PixLogic.UI/PixLogic/WindowScriptResult.cs
+++ PixLogic.UI/PixLogic/WindowScriptResult.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,29 @@ namespace PixLogic
         private void ExportBtn_Click(object sender, EventArgs e)
         {
 
+            using (SaveFileDialog exportDialog = new SaveFileDialog())
+            {
+                exportDialog.Filter = "Fichier CSV (*.csv)|*.csv" ;
+                exportDialog.FilterIndex = 2 ;
+                exportDialog.RestoreDirectory = true ;
+                if(exportDialog.ShowDialog() == DialogResult.OK)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    IEnumerable<string> columnNames = Queryresult.Columns.Cast<DataColumn>().
+                                                      Select(column => column.ColumnName);
+                    sb.AppendLine(string.Join(",", columnNames));
+
+                    foreach (DataRow row in Queryresult.Rows)
+                    {
+                        IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                        sb.AppendLine(string.Join(",", fields));
+                    }
+
+                    File.WriteAllText(exportDialog.FileName, sb.ToString());
+                }
+            }
+            
         }
     }
 }
